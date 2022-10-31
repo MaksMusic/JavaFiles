e65d58e05902e0b4812386695e2f24359595d1bd
maksm <Maksmusic05@yandex.ru>
2022-10-31 23:22:36 +0300

    addFile

--- src/AddFile.java
+++ src/AddFile.java
@@ -3,9 +3,7 @@ import java.io.File;
 public class AddFile {
     public static void main(String[] args) {
         File file = new File("C:\\Users\\maksm\\IdeaProjects\\JavaFiles");
-        String stokaOnline = "Hello";
-        stokaOnline = stokaOnline.substring(0, stokaOnline.length() - 1);
-        System.out.println(stokaOnline);
+
 
 
     }
