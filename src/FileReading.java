c00c9439773829ad5e691982f0f6a45b88eee84b
maksm <Maksmusic05@yandex.ru>
2022-11-02 00:39:34 +0300

    add FileReader sout file

--- src/FileReading.java
+++ src/FileReading.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.util.Arrays;
 
 public class FileReading {
-    public static void main1(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
 
         //прочитать файл
         //найти файл
@@ -19,24 +19,7 @@ public class FileReading {
     }
 
 
-    public static void main(String[] args) throws IOException{
-        fileReaderaddArray();
-    }
 
-    public static  void fileReaderaddArray() throws IOException {
 
-        char [] box = new char[25];
-        FileReader fileReader = new FileReader("C:\\Users\\maksm\\IdeaProjects\\JavaFiles\\src\\text.txt");
-        int c;
-
-        while ((c = fileReader.read(box)) > 0){
-            if(c < 25){
-                box = Arrays.copyOf(box,c);
-
-            }
-        }
-
-        System.out.println(Arrays.toString(box));
-    }
 }
 
