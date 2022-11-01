aff82c5751aa9e6876799d55c48cba35e4ae6aaa
maksm <Maksmusic05@yandex.ru>
2022-11-01 23:41:52 +0300

    add FileReader sout file

--- src/ReadingFile.java
+++ src/ReadingFile.java
@@ -5,10 +5,11 @@ public class ReadingFile {
     public static void main(String[] args) throws Exception {
         //найти файл
         FileReader fileReader = new FileReader("C:\\Users\\maksm\\IdeaProjects\\JavaFiles\\src\\text.txt");
-        int c;
+
 
         // в с принимаем каждый символ из файла в формате числа из таблицы юникод
         //если нет символа вернет -1
+        int c;
 
         while((c = fileReader.read()) != -1) {
             System.out.print((char)c);
