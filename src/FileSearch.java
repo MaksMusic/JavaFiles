6d0bcdf1f6c4f7e7fc44ba9855eb5120bf122c07
maksm <Maksmusic05@yandex.ru>
2022-11-09 00:00:22 +0300

    TecketShop version 2 add check

--- src/FileSearch.java
+++ src/FileSearch.java
@@ -5,18 +5,57 @@ import java.util.Arrays;
 public class FileSearch {
     public static void main(String[] args) {
 
-        File src = new File ("C:\\Users\\maksm\\IdeaProjects\\JavaFiles\\src");
+        // создать новый каталог
+        File file1 = new File("C:\\Users\\maksm\\IdeaProjects\\JavaFiles\\src\\new");
+        System.out.println(file1.mkdir() + " mkdir");     //вернут true если папка создастя
+
+        // создать новый файл
+        File fileNew = new File("C:\\Users\\maksm\\IdeaProjects\\JavaFiles\\src\\file.txt");
+        try {  System.out.println(fileNew.createNewFile()); }catch (Exception e){ System.out.println("false add file");}
+
+
+        //переименовать файл или категоию
+        File file2 = new File("C:\\Users\\maksm\\IdeaProjects\\JavaFiles\\src\\newCategory");
+        System.out.println(file1.renameTo(file2) + " rename");
+
+
+
+        //ссылка на каталог
+        File category = new File ("C:\\Users\\maksm\\IdeaProjects\\JavaFiles\\src");
+        //ссылка на файл
         File file = new File("C:\\Users\\maksm\\IdeaProjects\\JavaFiles\\src\\text.txt");
 
-        System.out.println(Arrays.toString(src.listFiles())); // показать список файлов
+
+        //работа с каталогами
+        System.out.println(Arrays.toString(category.list()));       // возвращает массив файлов и подкаталогов, типа String
+        System.out.println(Arrays.toString(category.listFiles())); // возвращает массив файлов и подкаталогов, типа File
+
+        //проверки файлов или катлогов
+        System.out.println(fileNew.exists() + "exist");
         System.out.println(file.isFile());                   //проверить является ли файлом или папкой
+        System.out.println(file.isDirectory());             //проверить является ли папкой или папкой
+        System.out.println(file.isHidden());                //проверить является ли файл скрытым
+
+        // данные о каталогах и файлах
+        System.out.println(file.getName());               //название файла и его тип
+        System.out.println(file.getParent());             // полный путь папки где находится папка или файл
+        System.out.println(file.lastModified());          // возвращает время последнего изменения файла или каталога
+        System.out.println(file.length());                //возвращает размер файла в байтах
 
 
-        //поиск файла по расширению
-        for (File fileStr : src.listFiles()) {
-            if (fileStr.getName().endsWith(".txt")){
+
+
+        searchFileType(category);
+    }
+
+
+    //поиск файла по расширению
+    public static void searchFileType(File category){
+        for (File fileStr : category.listFiles()) {
+            if ((fileStr != null) && (fileStr.getName().endsWith(".txt"))){
                 System.out.println(fileStr.getName());
             }
         }
+
     }
 }
