5f9ee613597c6d7f1962f718e41f581e3e7f4d9f
maksm <Maksmusic05@yandex.ru>
2022-11-15 23:54:47 +0300

    add fun listFilePrint

---
+++ src/FilePathFiles/Path/Path_Path.java
@@ -0,0 +1,12 @@
+package FilePathFiles.Path;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Path_Path {
+    public static void main(String[] args) {
+
+        Path path = new Paths("C:\\Users\\maksm\\IdeaProjects\\JavaFiles\\src\\FilePathFiles");
+
+    }
+}
